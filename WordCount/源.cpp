839a55b2e5c7226b1702ad9df72d08681db0c579
Yaya <email@1670024217.qq.com>
2022-05-29 01:34:19 +0800

    thrid

--- WordCount/源.cpp
+++ WordCount/源.cpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <stdio.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 #include <string>
@@ -14,12 +14,10 @@ int main()
 	if (!fp) {
 		printf("错误：文件读取失败\n");
 		return -1;
-
 	}
 
 	//对参数进行判断
-
-	if (!strcmp(b, "-c")) {//字符数
+	if (!strcmp(b, "-w")) {//字符数
 		char temp;
 		int num = 0;
 		temp = fgetc(fp);
@@ -27,13 +25,10 @@ int main()
 			//putchar(temp);
 			temp = fgetc(fp);
 			num++;
-
 		}
 		printf("该文本文件的字符数为：%d\n", num);
-
-
 	}
-	else if (!strcmp(b, "-w")) {//单词数
+	else if (!strcmp(b, "-c")) {//单词数
 		int w = 0;
 		char a = fgetc(fp);
 		while (a != EOF) {
@@ -42,16 +37,11 @@ int main()
 					a = fgetc(fp);
 				w++;
 				a = fgetc(fp);
-
-
 			}
 			else
 				a = fgetc(fp);
-
 		}
 		printf("该文本文件的单词数为：%d\n", w);
-
-
 	}
 	int n = fclose(fp);
 	system("pause");
